b7d470a1b07b5038eb4e94ee4bafc0d7a01d34dd
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2024-06-17 11:11:00 +0200

    sd

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,15 +2,15 @@
   <div class="flex flex-col gap-2 max-w-2xl m-auto my-16">
     <div class="flex items-center">
       <p class="text-neutral-500 min-w-28"><i class="pi pi-github" style="color: rgb(163 163 163);"></i> Github</p>
-      <a class="text-neutral-400" href="https://github.com/Edkiri">@Edkiri</a>
+      <a class="text-neutral-400 hover:text-neutral-300 hover:underline" href="https://github.com/Edkiri">@Edkiri</a>
     </div>
     <div class="flex items-center">
       <p class="text-neutral-500 min-w-28"><i class="pi pi-linkedin" style="color: rgb(163 163 163);"></i> Linkedin</p>
-      <a class="text-neutral-400" href="https://www.linkedin.com/in/edkiriakos">@edkiriakos</a>
+      <a class="text-neutral-400 hover:text-neutral-300 hover:underline" href="https://www.linkedin.com/in/edkiriakos">@edkiriakos</a>
     </div>
     <div class="flex items-center">
       <p class="text-neutral-500 min-w-28"><i class="pi pi-envelope" style="color: rgb(163 163 163);"></i> Email</p>
-      <p class="text-neutral-400" >eduardokiriakos@gmail.com</p>
+      <p class="text-neutral-400 hover:text-neutral-300 hover:underline" >eduardokiriakos@gmail.com</p>
     </div>
   </div>
 </template>
